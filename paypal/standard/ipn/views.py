9a12901ffac761382dec7365d91d4f36ac3bd9ec
ArtoLabs <artopium@gmail.com>
2022-01-13 15:18:17 +0100

    Can we print this file to the home directory?

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -68,7 +68,10 @@ def ipn(request):
 
     if data is not None:
 
-        file1 = open("test.log", "a")
+        from pathlib import Path
+        home = str(Path.home())
+        total_path = str('{}/paypal_test.log').format(home)
+        file1 = open(total_path, "w")
         file1.write(str('{}').format(data))
         file1.write(str('{}').format(dir(data)))
         file1.close()
